8d30fb0af89fbcd252e6d064172af2060e8f7073
Nadiia <sayalindemann@gmail.com>
2018-11-22 15:33:52 +0100

    added draft for transformation

---
+++ Tasks Collaboration/Data Transformation.R
@@ -0,0 +1,42 @@
+library(tidyverse)
+library(dplyr)
+
+# Read cleaned dataset from the input csv file
+file_path<-"Input Dataset/Cleaned Dataset/Supermarket_DataCleaned.csv"
+supermarket_data<-read_csv(file_path)
+
+## Converts data to tbl class. and tbl’s are easier to examine than data frames
+supermarket_tbl <- tbl_df(supermarket_data)
+supermarket_tbl
+
+# Select if average distance in row more then average distance in row
+view1 <- arrange(filter(supermarket_tbl, avg_distance_to_shops >= mean(supermarket_tbl$amount_purchased_total)), desc(avg_distance_to_shops))
+View(view1)
+
+# Show percent of situations, when customers traveled a longer then average distance 
+count1 <- nrow(supermarket_tbl)
+count2 <- nrow(view1)
+sprintf("%.0f%%", count2/count1 * 100)
+
+# Select a columns related to the distance and product purchased
+shop_distances <- supermarket_tbl[,3:7]
+View(shop_distances)
+
+View(t(shop_distances))
+
+shop_ordered_slice1 <- select(supermarket_tbl, 3,11,4,12,5,13,6,14,7,15)
+View(shop_ordered_slice1)
+
+
+# Select a columns related to the distance and unique product purchased
+shop_ordered_slice2 <- select(supermarket_tbl, 3,17,4,18,5,19,6,20,7,21)
+View(shop_ordered_slice2)
+
+# Count correlation between distance and product purchased 
+# cor(select(shop_ordered_slice1, 1,3,5,7,9), select(shop_ordered_slice1,2,4,6,8,10))
+ 
+# Count correlation between distance and product purchased 
+#cor(select(shop_ordered_slice2, 1,3,5,7,9), select(shop_ordered_slice2,2,4,6,8,10))
+
+# Count correlation between product purchased and unique product purchased
+#cor(select(shop_ordered_slice1, 2,4,6,8,10), select(shop_ordered_slice2,2,4,6,8,10))
