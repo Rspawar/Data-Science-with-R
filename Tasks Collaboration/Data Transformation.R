bd6940718f34a231e753098ccb411121958cffbc
Nadiia <sayalindemann@gmail.com>
2018-11-25 17:34:04 +0100

    extended slices in the data transformation
    added description file about existing slices

--- Tasks Collaboration/Data Transformation.R
+++ Tasks Collaboration/Data Transformation.R
@@ -8,7 +8,7 @@ cleared_supermarket_data<-read_csv(file_path)
 cleared_supermarked_tbl <- tbl_df(cleared_supermarket_data)
 cleared_supermarked_tbl
 
-# Select if average distance in row more then average distance in row
+# Select data when customers traveled a long distances
 view1 <- cleared_supermarked_tbl %>% 
   filter(avg_distance_to_all_shops >= mean(cleared_supermarked_tbl$avg_distance_to_all_shops)) %>%
   arrange(desc(avg_distance_to_all_shops))
@@ -20,10 +20,18 @@ count1 <- nrow(cleared_supermarked_tbl)
 count2 <- nrow(view1)
 sprintf("%.0f%%", count2/count1 * 100)
 
-# Select a columns related to the distance and product purchased
+# Select a columns related to the distance
 shop_distances <- cleared_supermarked_tbl[,3:7]
 View(shop_distances)
 
+# Select a columns related to the min, max and average distances 
+shop_distances_summaries <- cleared_supermarked_tbl[,8:10]
+
+# Combine two tables with distances
+shop_distances_ext <- bind_cols(shop_distances, shop_distances_summaries)
+View(shop_distances_ext)
+
+# Select a columns related to the distance and product purchased
 shop_ordered_slice1 <- select(cleared_supermarked_tbl, 3,11,4,12,5,13,6,14,7,15)
 View(shop_ordered_slice1)
 
@@ -34,14 +42,19 @@ View(shop_ordered_slice2)
 
 # Join slices by distance of shops
 unordered_bind <- shop_ordered_slice1 %>% 
-  bind_cols(shop_ordered_slice2[, grepl( "unique_products_purchased", names(shop_ordered_slice2))  ])
+  bind_cols(shop_ordered_slice2[, grepl( "unique_products_purchased", names(shop_ordered_slice2))])
                                                                    
 jnd_shop_ordered_products <- unordered_bind[,c(1,2,11,3,4,12,5,6,13,7,8,14,8,9,15)]
 View(jnd_shop_ordered_products)
 
 # Select segment from shops and prices
 shop_ordered_slice3 <- select(cleared_supermarked_tbl, 3,23,4,24,5,25,6,26,7,27)
-View(shop_ordered_slice3)
+#View(shop_ordered_slice3)
+
+# Select extended segment from shops and prices 
+shop_ordered_slice3_ext <- shop_ordered_slice3 %>% 
+  bind_cols(shop_distances_summaries, cleared_supermarked_tbl[,28])
+View(shop_ordered_slice3_ext)
 
 # Select segment from shops and average purchase
 shop_ordered_slice4 <- select(cleared_supermarked_tbl, 3,29,4,30,5,31,6,32,7,33)
