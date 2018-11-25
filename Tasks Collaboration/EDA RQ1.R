7e04857e50762caa1b7105093848e9a8da48481c
Nadiia <sayalindemann@gmail.com>
2018-11-25 23:48:57 +0100

    added colours for the last visualisation part

--- Tasks Collaboration/EDA RQ1.R
+++ Tasks Collaboration/EDA RQ1.R
@@ -103,9 +103,9 @@ joined_shops_data2 <- joined_shops_without_null %>%
 
 
 pall_3 <- ggplot(data = joined_shops_data2) + 
-  geom_point(mapping = aes(x = price, y = resid)) + ggtitle("Average price pattern")
+  geom_point(mapping = aes(x = price, y = resid), colour=colours_shema[6], alpha=0.3) + ggtitle("Average price pattern")
 
 # Once you’ve removed the strong relationship between distance and price, you can see what you expect in the relationship between shop and price relative other external factors (better quality of products, products alternative, shop location and so on)
 
 pall_4 <- ggplot(data = joined_shops_data2) + 
-  geom_boxplot(mapping = aes(x = Shop, y = resid)) + ggtitle("Average price pattern")
+  geom_boxplot(mapping = aes(x = Shop, y = resid), color=colours_shema[6]) + ggtitle("Average price pattern")
