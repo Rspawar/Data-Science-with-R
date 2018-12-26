3f4d7602bcd5e474663b367e0ba5d5fd75d0373a
KunduSumit <sumit@kundu.sumit>
2018-12-27 00:10:58 +0100

    Performed data wrangling on cleaned dataset and generated a dataset with labels, Modified code for feature selection

--- Tasks Collaboration/Sumit/Feature Selection.R
+++ Tasks Collaboration/Sumit/Feature Selection.R
@@ -1,15 +1,35 @@
-#### Boruta ####
+## Set file path
+file_path <- "Input Dataset/Cleaned Dataset/Supermarket_Data_Classification.csv"
+
+## Read data from a file
+supermarket_data_class <- read_csv(file_path)
+
+
+#### Feature selection using Boruta ####
 ## Install the package
 ##install.packages("Boruta")
 
-## Call the package
 library(Boruta)
 
 ## Set Pseudo Random Number Generator
 set.seed(1234)
 
 ## Implement Boruta
-cus_boruta <- Boruta(class ~ ., data = supermarket_data_clean, doTrace = 1)
+sm_boruta <- Boruta(class~., data = supermarket_data_class, doTrace = 1)
 
-## check the performance of Boruta
-print(cus_boruta)
+## Check the performance of Boruta
+print(sm_boruta)
+
+
+### Feature selection using Random Forest ###
+library(randomForest)
+library(caret)
+
+## Implement Random Forest
+sm_rf <- randomForest(class~., data = supermarket_data_class)
+
+## Generate the importance value for features 
+varImp(sm_rf)
+
+## Plot the importance value of features
+varImpPlot(sm_rf)
