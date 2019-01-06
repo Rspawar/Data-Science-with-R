273cae5159b15f4076b947e41e138438b0c44f45
sj2812 <45013757+sj2812@users.noreply.github.com>
2019-01-06 16:14:53 +0100

    EDARQ3.R visualisation shops vs avg distance and shops vs avg amount purchased RQ3.R algorithms svm naive bayes and decision trees

--- Tasks Collaboration/Shivani/RQ3.R
+++ Tasks Collaboration/Shivani/RQ3.R
@@ -1,37 +1,47 @@
-library(tidyverse)
-library(ggplot2)
-library("RColorBrewer")
-library(readr)
-library(dplyr)
+
 library(e1071)
 library (caret)
+library(rpart)
 
 file_path<- "Input Dataset/Cleaned Dataset/Supermarket_Data_Prediction.csv"
 supermarket_data_predict <- read_csv(file_path)
 View(supermarket_data_predict)
 supermarket_data_predict$most_pref_shop=factor(supermarket_data_predict$most_pref_shop)
 str(supermarket_data_predict)
-#split data into 70% training dataset and 30% test dataset
-## set the seed to make your partition reproducible
-set.seed(7267166)
-trainIndex=createDataPartition(supermarket_data_predict$most_pref_shop, p=0.7)$Resample1
-train=mydata[trainIndex, ]
-test=mydata[-trainIndex, ]
+## 75% of the sample size
+smp_size <- floor(0.75 * nrow(supermarket_data_predict))
 
+## set the seed to make your partition reproducible
+set.seed(123)
+train_ind <- sample(seq_len(nrow(supermarket_data_predict)), size = smp_size)
+
+train <- supermarket_data_predict[train_ind, ]
+test <- supermarket_data_predict[-train_ind, ]
+
+svmmodel=svm(most_pref_shop ~., data=train, method="class")
+
+#decision tree classifier
+modelr = rpart(most_pref_shop ~., data=train, method="class")
 
 #naive bayes classifier
-NBclassfier=naiveBayes(most_pref_shop~., data=train)
-print(NBclassfier)
-trainPred=predict(NBclassfier,newdata =train, type = "class")
-trainTable=table(train$most_pref_shop, trainPred)
-testPred=predict(NBclassfier, newdata=test, type="class")
-testTable=table(test$most_pref_shop, testPred) 
-trainAcc=(trainTable[1,1]+trainTable[2,2]+trainTable[3,3])/sum(trainTable)
-testAcc=(testTable[1,1]+testTable[2,2]+testTable[3,3])/sum(testTable)
-message("Contingency Table for Training Data")
-message("Contingency Table for Test Data")
-print(testTable)
-message("Accuracy")
-print(round(cbind(trainAccuracy=trainAcc, testAccuracy=testAcc),3))
+NBclassfier=naiveBayes(most_pref_shop ~., data=train)
+#print(NBclassfier)
+
+#function to calculate accuracy of different models
+printALL=function(model,name){
+  trainPred=predict(model,newdata =train, type = "class")
+  trainTable=table(train$most_pref_shop, trainPred)
+  testPred=predict(model, newdata=test, type="class")
+  testTable=table(test$most_pref_shop, testPred) 
+  trainAcc=(trainTable[1,1]+trainTable[2,2]+trainTable[3,3])/sum(trainTable)
+  testAcc=(testTable[1,1]+testTable[2,2]+testTable[3,3])/sum(testTable)
+  #message("Contingency Table for Training Data")
+  #message("Contingency Table for Test Data")
+  #print(testTable)
+  message(name)
+  print(round(cbind(trainAccuracy=trainAcc, testAccuracy=testAcc),3))
+}
+printALL(NBclassfier,"naive bayes")
+printALL(modelr,"decision trees")
 
 
