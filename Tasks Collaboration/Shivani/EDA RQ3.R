af4234b5268649f6e0fe9b18d001c59deca4b2fb
sj2812 <shivani.jadhav2812@gmail.com>
2019-01-13 16:01:59 +0100

    modified graphs

--- Tasks Collaboration/Shivani/EDA RQ3.R
+++ Tasks Collaboration/Shivani/EDA RQ3.R
@@ -13,34 +13,8 @@ View(supermarket_data_predict)
 
 #plotting based on all combinations of columns
 ggplot(supermarket_data_predict, aes(x=supermarket_data_predict$distance_avg, y=supermarket_data_predict$products_purchased_avg,colour=supermarket_data_predict$most_pref_shop)) +
-  geom_point(shape=1, aes(color = factor(supermarket_data_predict$most_pref_shop))) +scale_x_discrete(name="Average distance")+scale_y_continuous(name="Average products purchased")
+  geom_point(shape=1, aes(color = factor(supermarket_data_predict$most_pref_shop))) +scale_x_discrete(name="Average distance")+scale_y_continuous(name="Average products purchased")+labs(color="Shops")
 
 ggplot(supermarket_data_predict, aes(x=supermarket_data_predict$products_purchased_avg, y=supermarket_data_predict$unique_products_purchased_avg)) +
-  geom_point(shape=1, aes(color = factor(supermarket_data_predict$most_pref_shop))) +scale_x_discrete(name="Average products purchased")+scale_y_continuous(name="Average unique products purchased")
+  geom_point(shape=1, aes(color = factor(supermarket_data_predict$most_pref_shop))) +scale_x_discrete(name="Average products purchased")+scale_y_continuous(name="Average unique products purchased")+labs(color="Shops")
 
-ggplot(supermarket_data_predict, aes(x=supermarket_data_predict$distance_avg, y=supermarket_data_predict$unique_products_purchased_avg)) +
-  geom_point(shape=1, aes(color = factor(supermarket_data_predict$most_pref_shop))) 
-
-ggplot(supermarket_data_predict, aes(x=supermarket_data_predict$distance_avg, y=supermarket_data_predict$product_price_avg)) +
-  geom_point(shape=1, aes(color = factor(supermarket_data_predict$most_pref_shop))) 
-
-ggplot(supermarket_data_predict, aes(x=supermarket_data_predict$distance_avg, y=supermarket_data_predict$amount_purchased_avg)) +
-  geom_point(shape=1, aes(color = factor(supermarket_data_predict$most_pref_shop))) 
-
-ggplot(supermarket_data_predict, aes(x=supermarket_data_predict$products_purchased_avg, y=supermarket_data_predict$product_price_avg,colour=supermarket_data_predict$most_pref_shop)) +
-  geom_point(shape=1, aes(color = factor(supermarket_data_predict$most_pref_shop))) 
-
-ggplot (supermarket_data_predict, aes (x = supermarket_data_predict$products_purchased_avg, y = supermarket_data_predict$product_price_avg, fill = supermarket_data_predict$most_pref_shop)) + stat_binhex (bins=5, aes (alpha = ..count..)) + facet_grid (. ~ supermarket_data_predict$most_pref_shop)
-
-
-ggplot(supermarket_data_predict, aes(x=supermarket_data_predict$products_purchased_avg, y=supermarket_data_predict$amount_purchased_avg)) +
-  geom_point(shape=1, aes(color = factor(supermarket_data_predict$most_pref_shop))) 
-
-ggplot(supermarket_data_predict, aes(x=supermarket_data_predict$unique_products_purchased_avg, y=supermarket_data_predict$product_price_avg)) +
-  geom_point(shape=1, aes(color = factor(supermarket_data_predict$most_pref_shop))) 
-
-ggplot(supermarket_data_predict, aes(x=supermarket_data_predict$unique_products_purchased_avg, y=supermarket_data_predict$amount_purchased_avg)) +
-  geom_point(shape=1, aes(color = factor(supermarket_data_predict$most_pref_shop))) 
-
-ggplot(supermarket_data_predict, aes(x=supermarket_data_predict$product_price_avg, y=supermarket_data_predict$amount_purchased_avg)) +
-  geom_point(shape=1, aes(color = factor(supermarket_data_predict$most_pref_shop))) 
